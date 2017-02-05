fe1557d31c3c75721268c25ef7d989eb2f5b6e98
Michael Bumann <hello@michaelbumann.com>
2017-02-05 22:12:14 +0800

    bye es6

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Contracts = require('./lib/contracts.js');
 
 module.exports = function (web3) {
-  let contracts = {};
-  Object.keys(Contracts).forEach((contractName) => {
+  var contracts = {};
+  Object.keys(Contracts).forEach(function (contractName) {
     contracts[contractName] = web3.eth.contract(Contracts[contractName].abi).at(Contracts[contractName].address);
   });
   return contracts;
