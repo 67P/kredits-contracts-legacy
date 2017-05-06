bb6592b1e392207f8c7d7b37c8ef1a2730cc313b
Michael Bumann <hello@michaelbumann.com>
2017-05-06 17:42:42 +0200

    Add support to overwrite config defaults

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var abi = require('./lib/abi.js');
-var addresses = require('./lib/address.js');
+const abi = require('./lib/abi.js');
+const addresses = require('./lib/address.js');
 
-module.exports = function (web3, overwriteAddresses, overwriteAbi) {
-  var contractAddresses = overwriteAddresses || addresses;
-  var contractAbi = overwriteAbi || abi;
-  var contracts = {};
-
+module.exports = function (web3, opts = {}) {
+  let contracts = {};
   Object.keys(abi).forEach(function (contractName) {
-    contracts[contractName] = web3.eth.contract(contractAbi[contractName]).at(contractAddresses[contractName]);
+    let config = {
+      address: addresses[contractName],
+      abi: abi[contractName]
+    };
+
+    let contractOpts = opts[contractName];
+    ['address', 'abi'].forEach(m => {
+      if (contractOpts && contractOpts[m]) {
+        config[m] = contractOpts[m];
+      }
+    });
+
+    contracts[contractName] = web3.eth.contract(config.abi).at(config.address);
   });
+
   return contracts;
 };
