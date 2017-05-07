cd3b0b6cdcb6a3eea975fe352cba8d4d3070f65e
Michael Bumann <hello@michaelbumann.com>
2017-05-07 22:44:55 +0200

    Support development metadata files in kredits-inspect

--- scripts/inspect.js
+++ scripts/inspect.js
@@ -2,6 +2,7 @@
 
 const program = require('commander');
 const path = require('path');
+const fs = require('fs');
 const pkg = require(path.join(__dirname, '../package.json'));
 
 program
@@ -9,15 +10,32 @@ program
   .option('-w, --what <abi|address|bytecode>', 'what to inspect. default: address')
   .option('-c, --contracts <Contract,Names>', 'comma sparated list of contracts to deploy. default: all contracts found in the meatadata file', function (val) { return val.split(','); })
   .option('-r, --raw', 'do not print contract names (when piping output somewhere)')
+  .option('-d, --directory <path/to/directory>', 'direcoty path where to find the metadata files.')
   .parse(process.argv);
 
 let what = program.what || 'address';
 
-const Metadata = require(path.join(__dirname, '..', `lib/${what}.js`));
+let filePath;
+
+if (program.directory) {
+  filePath = path.join(program.directory, `${what}.js`);
+} else {
+  if (fs.existsSync(path.join(__dirname, '..', `tmp/${what}.js`))) {
+    filePath = path.join(__dirname, '..', `tmp/${what}.js`);
+  } else {
+    filePath = path.join(__dirname, '..', `lib/${what}.js`);
+  }
+}
+const Metadata = require(filePath);
 let contractNames = program.contracts || Object.keys(Metadata);
 
 contractNames.forEach((c) => {
-  let output = JSON.stringify(Metadata[c]);
+  let output;
+  if (typeof Metadata[c] === 'string') {
+    output = Metadata[c];
+  } else {
+    output = JSON.stringify(Metadata[c]);
+  }
   if (program.raw) {
     console.log(output);
   } else {
