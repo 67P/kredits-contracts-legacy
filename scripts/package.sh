8599355e9d010328e8427d582dead4aeb30edd32
Sebastian Kippe <sebastian@kip.pe>
2017-05-10 14:54:09 +0200

    npm scripts ftw

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,12 +3,8 @@
 DIR=$(dirname `realpath $0`)
 ROOT_DIR="$DIR/.."
 
-echo "Copying development contract data from $ROOT_DIR/lib/dev to $ROOT_DIR/lib"
-
 set +xe
 cp $ROOT_DIR/lib/dev/*.js $ROOT_DIR/lib
 set -e
 
-echo ""
-echo "Done. Do not forget to commit and release a new version of the package"
-
+echo "Copied development contract data from $ROOT_DIR/lib/dev to $ROOT_DIR/lib"
