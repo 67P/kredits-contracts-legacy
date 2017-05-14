95e19d705e7348236f3ad13809b9a5f78d5ab23c
bumi <hello@michaelbumann.com>
2017-05-14 20:40:27 +0200

    Remove contracts bytecode
    Currently we do not need the raw contract bytecode. The bytecode is
    mainly helpful for contract deployments.
    Removing the bytecode makes the package smaller.

--- scripts/inspect.js
+++ scripts/inspect.js
@@ -7,7 +7,7 @@ const pkg = require(path.join(__dirname, '../package.json'));
 
 program
   .version(pkg.version)
-  .option('-w, --what <abi|address|bytecode>', 'what to inspect. default: address')
+  .option('-w, --what <abi|address>', 'what to inspect. default: address')
   .option('-c, --contracts <Contract,Names>', 'comma sparated list of contracts to deploy. default: all contracts found in the meatadata file', function (val) { return val.split(','); })
   .option('-r, --raw', 'do not print contract names (when piping output somewhere)')
   .option('-d, --directory <path/to/directory>', 'absolute(!) direcoty path where to find the metadata files.')
