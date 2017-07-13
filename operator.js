36da9bf4b7777018b8732b2c2a1763c653ad028b
Sebastian Kippe <sebastian@kip.pe>
2017-07-13 16:47:48 +0200

    Add new wrapper for kredits clients to use
    This adds a wrapper for just the operator contract, including a new
    constructor as well as wrapper functions for getting/setting
    contributors.

---
+++ operator.js
@@ -0,0 +1,60 @@
+/* global module, Buffer */
+const abi = require('./lib/abi.js');
+const addresses = require('./lib/address.js');
+const base58 = require('base-58');
+
+module.exports = function(web3, contractOpts = {}) {
+  this.networkId = contractOpts['networkId'] || web3.version.network;
+  this.contractName = 'Operator';
+
+  this.config = {
+    address: addresses[this.contractName][this.networkId],
+    abi: abi[this.contractName][this.networkId]
+  };
+
+  ['address', 'abi'].forEach(m => {
+    if (contractOpts && contractOpts[m]) {
+      this.config[m] = contractOpts[m];
+    }
+  });
+  if(!this.config.abi) { throw 'ABI not found for contract: ' + this.contractName; }
+  if(!this.config.address) { throw 'address not found for contract: ' + this.contractName;}
+
+  this.contract = web3.eth.contract(this.config.abi).at(this.config.address);
+
+  this.getValueFromContract = function(contractMethod, ...args) {
+    return new Promise((resolve, reject) => {
+      this.contract[contractMethod](...args, (err, data) => {
+        if (err) { reject(err); }
+        resolve(data);
+      });
+    });
+  };
+
+  this.addContributor = function(address, profileHashBase58, isCore) {
+    const profileHashHex = Buffer.from(base58.decode(profileHashBase58))
+                                 .slice(2) // strip first two bytes
+                                 .toString('hex');
+
+    return new Promise((resolve, reject) => {
+      this.contract.addContributor(address, '0x'+profileHashHex, isCore, (err, res) => {
+        if (err) { reject(err); } else { resolve(res); }
+      });
+    });
+  };
+
+  this.getContributor = function(id) {
+    return this.getValueFromContract('contributors', id).then(res => {
+      const profileHashHex = '1220' + res[1].replace('0x','');
+      const profileHashBase58 = base58.encode(new Buffer(profileHashHex, 'hex'));
+      const contributor = {
+        id: id,
+        address: res[0],
+        profileHash: profileHashBase58,
+        isCore: res[2]
+      };
+      return contributor;
+    });
+  };
+
+};
